348a90f2e5e0b39739b680c3653f8c68ab2b34d3
bardrock01 <jaitlysankalp@gmail.com>
2022-08-20 16:08:30 +0530

    Settuped the background

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./App.scss"
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div className="main">
+          <div className="ImageContainer"
+            style={{ backgroundImage: "url(/image.png)" }}
+          ></div>
+          <div className="">
+
+          </div>
+        </div>
       </header>
     </div>
   );
