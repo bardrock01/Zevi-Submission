92a38f3cb6f5c934f70ac12ee12c2709bf940b48
bardrock01 <jaitlysankalp@gmail.com>
2022-08-20 17:29:53 +0530

    Changed Border of Search box

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-import "./App.scss"
-
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      
-        <div className="main">
-          <div className="ZeviName">
-            <img src="/my-app/src/assets/img/zevi.png" className="ZeviImageTag" alt=""/>
-          </div>
-          <div className="SearchBar">
-            <input type= "text" placeholder="Search" className="search">
-            </input>
-          </div>
+      <div className="main">
+        <div className="ZeviName">
+          <img
+            src="/my-app/src/assets/img/zevi.png"
+            className="ZeviImageTag"
+            alt="Zevi"
+          />
         </div>
-      
+        <div className="SearchBar">
+          <input type="text" placeholder="Search" className="search"></input>
+        </div>
+      </div>
     </div>
   );
 }
